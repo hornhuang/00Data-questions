ef3ad0f8b6e4a9d6b4d8fb6b4692a54ac3179a00
FishInWater-1999 <814484626@qq.com>
2020-03-31 10:12:12 +0800

    add Accessibility Service

---
+++ app/src/main/java/com/fishinwater/a00data_questions/accessibility/service/AccessibilityService.java
@@ -0,0 +1,10 @@
+package com.fishinwater.a00data_questions.accessibility.service;
+
+import android.app.Service;
+
+/**
+ * @author fishinwater-1999
+ * @date :2020/3/31 10:11
+ */
+public class AccessibilityService extends Service {
+}
