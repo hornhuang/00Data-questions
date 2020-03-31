8a887677866d76a8a5fdbe4507d4efc52a75c284
FishInWater-1999 <814484626@qq.com>
2020-03-31 10:17:27 +0800

    add auto_reply_service_config

--- app/src/main/java/com/fishinwater/a00data_questions/accessibility/service/AccessibilityService.java
+++ app/src/main/java/com/fishinwater/a00data_questions/accessibility/service/AccessibilityService.java
@@ -1,10 +1,27 @@
 package com.fishinwater.a00data_questions.accessibility.service;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.view.accessibility.AccessibilityEvent;
+
+import androidx.annotation.Nullable;
 
 /**
  * @author fishinwater-1999
  * @date :2020/3/31 10:11
  */
-public class AccessibilityService extends Service {
+public class AccessibilityService extends android.accessibilityservice.AccessibilityService {
+
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
 }
